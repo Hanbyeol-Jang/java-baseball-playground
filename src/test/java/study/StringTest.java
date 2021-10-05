4f1cd84200580700c63c4bf7673a6192fe928f31
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2021-10-05 23:08:42 +0900

    [학습 TEST] String Class
    assertThat()
    @Test
    @DisplayName

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,37 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
     @Test
+    @DisplayName("[TEST] replace")
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    /* Req.1 */
+    @Test
+    @DisplayName("[TEST] split")
+    void split() {
+        String s = "1,2";
+        String[] arr = s.split(",");
+
+        assertThat(arr).contains("1");
+        assertThat(arr).containsExactly("1");
+    }
+
+    /* Req.2 */
+    @Test
+    @DisplayName("[TEST] substring")
+    void substring() {
+        String s = "(1,2)";
+        String sub = s.substring(1, s.length() - 1);
+
+        assertThat(sub).isEqualTo("1,2");
+    }
+
 }
