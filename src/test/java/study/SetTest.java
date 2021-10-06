e220aac8873c0ce3537880d322b72280c837f801
Hanbyeol-Jang <35832112+Hanbyeol-Jang@users.noreply.github.com>
2021-10-06 23:20:24 +0900

    [학습 TEST] SetTest

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,40 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    /* Req.1 */
+    @Test
+    @DisplayName("[TEST] size")
+    void size(){
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    /* Req.2 */
+    @Test
+    @DisplayName("[TEST] contains")
+    void contains(){
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+}
